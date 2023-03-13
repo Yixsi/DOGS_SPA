efe4629d41c8877967a3172c65e3e4c5179c5fc5
Yixsi <yixsi3103@gmail.com>
2023-03-13 15:17:20 -0500

    Adding model

--- README.md
+++ README.md
@@ -245,5 +245,6 @@ Ten en cuenta que en esta instancia no es obligatorio el desarrollo de testing p
 <br />
 
 <img src="./dogs.jpg" alt="" width="1000px" />
-#   D O G S _ S P A  
+#   D O G S _ S P A 
+ 
  
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,31 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 };
