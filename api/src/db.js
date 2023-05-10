eb82a33f2375439890602dff63e6f6311d134209
Yixsi <yixsi3103@gmail.com>
2023-05-10 11:40:32 -0500

    Render deploy

--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL_RENDER } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+const sequelize = new Sequelize(DB_URL_RENDER || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
